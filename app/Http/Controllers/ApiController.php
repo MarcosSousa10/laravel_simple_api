fb406d1002ce578c3ee40c8ca876cceb93102670
Marcos <marcospegodesousa10@gmail.com>
2024-10-04 11:16:54 -0300

    get todos os clientes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     public function  status()
     {
-        return ('OK');
+        return response()->json(
+            [
+                'status' => 'ok',
+                'message' => 'Api is running OK!'
+            ],
+            200
+        );
     }
     public function  clients()
     {
-        return ('all');
+        $clients = Client::all();
+        return response()->json(
+            [
+                'status' => 'ok',
+                'message' => 'sucesso',
+                'data' => $clients
+            ],
+            200
+        );
     }
 }
